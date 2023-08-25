ea6bb99281a70013fcc13d10fd44a104adc5f8e8
visionjain <visionjain118@gmail.com>
2023-08-26 02:31:16 +0530

    fix2

--- src/components/Customers/customers.jsx
+++ src/components/Customers/customers.jsx
@@ -123,7 +123,7 @@ const Customers = ({customer}) => {
                     </div>
                     <div className="mt-3 md:mt-0">
                         <a
-                            className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
+                            className="cursor-pointer inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
                             onClick={handleAddDataClick}
                         >
                             Add Customer
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -580,7 +580,7 @@ const Landing = () => {
           </div>
           <div className="mt-3 md:mt-0">
             <a
-              className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
+              className="cursor-pointer inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
               onClick={handleAddDataClick}
             >
               Add Data
