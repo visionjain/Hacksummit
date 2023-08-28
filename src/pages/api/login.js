eba258f1c7df9afe142203fab18e77d1e2e776e4
Vision Jain <visionjain118@gmail.com>
2023-08-28 23:34:08 +0530

    ss

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -10,6 +10,6 @@ export default async function handler(req, res) {
     if (!user) {
         return res.json({ status: 'User does not exist' });
     } else {
-        return res.json({ status: 'Success', redirectUrl: '/customers' });
+        res.redirect('/customers')
     }
 }
