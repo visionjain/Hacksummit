a55f8479f6594582c3b6a8bf14a2d165ce6096b0
Vision Jain <visionjain118@gmail.com>
2023-08-28 23:29:33 +0530

    oro

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,13 +3,13 @@ import User from '../../../model/loginschema'
 
 connect()
 
-export default async function handler(req,res){
-    const {email,password}=req.body
-    const user = await User.findOne({email,password})
-    if(!user){
-        return res.json({status: 'User doesnt exist'})
-    }
-    else{
-        res.redirect('/customers')
+export default async function handler(req, res) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+        return res.json({ status: 'User does not exist' });
+    } else {
+        return res.json({ status: 'Success', redirectUrl: '/customers' });
     }
 }
