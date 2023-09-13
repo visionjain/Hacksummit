76da256765a4d454c17ed18f9b9c5aa42489d7f0
Vision Jain <visionjain118@gmail.com>
2023-09-13 22:23:48 +0530

    sfgdbjfshbf

--- src/pages/customers/[customerid]/index.js
+++ src/pages/customers/[customerid]/index.js
@@ -512,63 +512,63 @@ const Landing = () => {
                                 {customer && customer.data && paginatedTableItems
                                     ? paginatedTableItems.map((item, idx) => (
                                         <tr key={idx} className="divide-x">
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold"> {item.numberid === '' ? '-' : item.numberid}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold"> {item.salesdate === '' ? '-' : item.salesdate}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl"> {item.numberid === '' ? '-' : item.numberid}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl"> {item.salesdate === '' ? '-' : item.salesdate}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.drivername === '' ? '-' : item.drivername}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">{item.autono === '' ? '-' : item.autono}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">{item.autono === '' ? '-' : item.autono}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.Limea ? (
                                                     `${item.Limea} X ${item.LimeaPrice}`
                                                 ) : '-'}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.Limew ? (
                                                     `${item.Limew} X ${item.LimewPrice}`
                                                 ) : '-'}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.Limeb ? (
                                                     `${item.Limeb} X ${item.LimebPrice}`
                                                 ) : '-'}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.Limeoffw ? (
                                                     `${item.Limeoffw} X ${item.LimeoffwPrice}`
                                                 ) : '-'}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.jhiki ? (
                                                     `${item.jhiki} X ${item.jhikiPrice}`
                                                 ) : '-'}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.rs ? (
                                                     `${item.rs} KG X ${item.rsPrice}`
                                                 ) : '-'}
                                             </td>
 
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.siteaddress === '' ? '-' : item.siteaddress}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {isNaN(item.amount) || item.amount === '' ? '0' : parseFloat(item.amount).toFixed(2)}
                                             </td>
 
 
-                                            <td className="px-2 py-4 whitespace-nowrap font-bold text-center">{item.labourcharge === '' ? '-' : item.labourcharge}</td>
+                                            <td className="px-2 py-4 whitespace-nowrap font-bold text-center text-5xl">{item.labourcharge === '' ? '-' : item.labourcharge}</td>
                                             <td className="px-2 py-4 whitespace-nowrap font-bold text-center text-5xl">{item.autocharge === '' ? '-' : item.autocharge}</td>
 
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">{item.dr}</td>
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">{item.dr}</td>
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.cr === '' ? 0 : item.cr}
                                             </td>
 
-                                            <td className="px-6 py-4 whitespace-nowrap font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap font-bold text-5xl">
                                                 {item.balance < 0 ? `${Math.abs(item.balance)} ADV` : item.balance}
                                             </td>
-                                            <td className="px-6 py-4 whitespace-nowrap print:hidden font-bold">
+                                            <td className="px-6 py-4 whitespace-nowrap print:hidden font-bold text-5xl">
                                                 <button
                                                     onClick={() => handleViewData(customer.customerid, item._id)}
                                                     className="px-4 py-2 text-white bg-green-600 rounded-lg duration-150 hover:bg-green-700 active:shadow-lg font-bold"
@@ -576,7 +576,7 @@ const Landing = () => {
                                                     Generate Bill
                                                 </button>
                                             </td>
-                                            <td className="text-right px-6 whitespace-nowrap print:hidden font-bold">
+                                            <td className="text-right px-6 whitespace-nowrap print:hidden font-bold ">
                                                 <button
                                                     onClick={() => handleEditClick(idx, item.numberid)} // Call handleEditClick with the index
                                                     className="py-2 px-3 font-medium text-indigo-600 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg"
