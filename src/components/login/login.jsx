69ecaa2ef90d1e7e5deac0158deb338b55a1d6a5
Vision Jain <visionjain118@gmail.com>
2023-09-15 19:21:12 +0530

    sdfsd

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,41 +2,31 @@ import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 import logoagri from '../../../public/logoagri.png'
+import pattaBackground from "../../../public/patta.jpg"
 
 const Login = () => {
+    const backgroundStyle = {
+        backgroundImage: `url(${pattaBackground.src})`, // Use .src to get the image URL
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '750px', // Add a height to the div to make sure it's visible
+    };
     return (
         <div>
             <main className="w-full flex">
                 <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
                     <div className="relative z-10 w-full max-w-md">
-                        <Image
-                            src={logoagri}
-                            width={350}
-                            height={400}
-                            alt="Logo"
-                        />
+                        
                         <div className=" mt-16 space-y-3">
                             <h3 className="text-white text-3xl font-bold">Start growing your business quickly</h3>
                             <p className="text-gray-300">
                                 Create an account and get access to all features for 30-days, No credit card required.
                             </p>
-                            {/* <div className="flex items-center -space-x-2 overflow-hidden">
-                                <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://randomuser.me/api/portraits/men/86.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
-                                <p className="text-sm text-gray-400 font-medium translate-x-5">
-                                    Join 5.000+ users
-                                </p>
-                            </div> */}
                         </div>
                     </div>
                     <div
                         className="absolute inset-0 my-auto h-[500px]"
-                        style={{
-                            background: "linear-gradient(152.92deg, rgb(168, 232, 144) 4.54%, rgb(168, 232, 144) 34.2%, #749F82 77.55%)", filter: "blur(118px)"
-                        }}
+                        style={backgroundStyle} 
                     >
 
                     </div>
@@ -44,6 +34,13 @@ const Login = () => {
                 <div className="flex-1 flex items-center justify-center h-screen">
                     <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
                         <div className="">
+                            <Image
+                            src={logoagri}
+                            width={350}
+                            height={400}
+                            alt="Logo"
+                            className='ml-14'
+                        />
                             <div className="mt-5 space-y-2">
                                 <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Log in</h3>
 
@@ -143,70 +140,6 @@ const Login = () => {
                 </div>
             </main>
         </div>
-        // <div>
-        //     <div>
-        //         <Image
-        //             src={jlc}
-        //             width={150}
-        //             height={200}
-        //             className='absolute md:ml-10 md:w-40 w-24 ml-36'
-        //             alt="Logo"
-        //         />
-        //     </div>
-        //     <main className="w-full h-screen flex flex-col items-center justify-center px-4">
-        //         <div className="max-w-sm w-full text-gray-600 space-y-8">
-        //             <div className="text-center">
-        //                 <Image
-        //                     src="/loginicon.png"
-        //                     width={150}
-        //                     height={150}
-        //                     className='mx-auto'
-        //                     alt="Logo"
-        //                 />
-
-        //                 <div className="mt-5 space-y-2">
-        //                     <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Log in to your account</h3>
-        //                 </div>
-        //             </div>
-        //             <form
-        //                 action="/api/login" method='post'
-        //             >
-        //                 <div>
-        //                     <label className="font-medium">
-        //                         Username
-        //                     </label>
-        //                     <input
-        //                         type="userid"
-        //                         name='userid'
-        //                         placeholder='Type your username'
-        //                         required
-        //                         className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-        //                     />
-        //                 </div>
-        //                 <div>
-        //                     <label className="font-medium">
-        //                         Passoword
-        //                     </label>
-        //                     <input
-        //                         type="password"
-        //                         name='password'
-        //                         placeholder='Type your password'
-        //                         required
-        //                         className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-        //                     />
-        //                 </div>
-        //                 {/* <Link href='/customers'> */}
-        //                 <button
-        //                     className="w-full mt-4 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150 cursor-pointer"
-        //                     type='submit' value='Login'
-        //                 >
-        //                     Log In
-        //                 </button>
-        //                 {/* </Link> */}
-        //             </form>
-        //         </div>
-        //     </main>
-        // </div>
     )
 }
 
--- src/pages/selector.js
+++ src/pages/selector.js
@@ -12,21 +12,110 @@ const Selector = () => {
         router.push('/labours');
     };
 
+    const headingStyle = {
+        background: 'linear-gradient(95deg, #F0BF12 4.71%, #F98214 81.93%)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+    };
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold mb-6">Select Dashboard</h1>
-            <button
-                className="bg-blue-500 hover:bg-blue-600 text-white text-2xl font-bold p-10 px-20 rounded mb-4"
-                onClick={handleCustomerClick}
+        <div className="flex flex-row items-center justify-center h-screen bg-black">
+            <div
+                className='bg-green-800 ml-10 text-white w-[600px] h-[600px] rounded-3xl text-center justify-center'
+                style={{
+                    backgroundImage: 'linear-gradient(313deg, rgba(249, 130, 20, 0.1) -15.02%, rgba(20, 249, 130, 0.4) 137.34%), url("https://kj1bcdn.b-cdn.net/media/10157/modern-harvesting-machines.jpg")',
+                    backgroundSize: 'cover', // Ensure the image covers the entire element
+                    backgroundPosition: 'center', // Center the image
+                    opacity: '0.7', // Adjust the opacity (0.7 means 70% opacity)
+                }}
             >
-                Customers
-            </button>
-            <button
-                className="bg-green-500 hover:bg-green-600 text-white text-2xl font-bold p-10 px-24 rounded mt-4"
-                onClick={handleLabourClick}
+                <div className='text-3xl mt-10 text-yellow-400 font-bold ml-32 w-[60%]' style={headingStyle}>
+                    Our High Quality Organic Products
+                </div>
+                <div class='w-96 ml-28 mt-20'>We offer premium, all-natural products of the highest quality, cultivated through organic farming practices. Our commitment to organic standards ensures that our customers receive the healthiest and most environmentally-friendly options available. Experience the pure and unadulterated goodness of our organic offerings today.</div>
+                <button className='absolute bottom-[100px] left-96'>
+                    <a
+                        className="group relative inline-flex items-center overflow-hidden rounded px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                        href="https://visionjain.github.io/Microsoft-future-ready-talent-project/"
+                        style={{
+                            background: 'linear-gradient(313deg, #F98214 -15.02%, rgba(249, 130, 20, 0.00) 137.34%)',
+                            borderRadius: '66px',
+                            border: '2px solid #EA760C',
+                        }}
+                    >
+                        <span className="absolute -end-full transition-all group-hover:end-4">
+                            <svg
+                                className="h-5 w-5 rtl:rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                />
+                            </svg>
+                        </span>
+
+                        <span className="text-sm font-medium transition-all group-hover:me-4">
+                            Know More
+                        </span>
+                    </a>
+                </button>
+
+
+            </div>
+            <div
+                className='bg-green-800 ml-10 text-white w-[600px] h-[600px] rounded-3xl text-center justify-center'
+                style={{
+                    backgroundImage: 'linear-gradient(313deg, rgba(249, 130, 20, 0.4) -15.02%, rgba(20, 249, 130, 0.4) 137.34%), url("https://nicholas.duke.edu/sites/default/files/styles/banner/public/images/energyiStock-952105020_0.jpg?h=f7dc1c74&itok=Ge8YAJLm")',
+                    backgroundSize: 'cover', // Ensure the image covers the entire element
+                    backgroundPosition: 'center', // Center the image
+                }}
             >
-                Labours
-            </button>
+                <div className='text-3xl mt-10 text-yellow-400 font-bold ml-32 w-[60%]' style={headingStyle}>
+                    Make an Energy-Efficient Home With Us
+                </div>
+                <div class='w-96 ml-28 mt-20'>Partner with us to create your energy-efficient dream home. Our experts use sustainable materials and innovative designs to maximize energy efficiency, reduce carbon footprint, and lower utility costs. Join us in building a comfortable, eco-friendly sanctuary that benefits your wallet and the planet.</div>
+
+                <div>
+                    <button className='absolute bottom-[100px] right-[370px]' href="https://visionjain.github.io/Microsoft-future-ready-talent-project/">
+                        <a
+                            className="group relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                            href="https://visionjain.github.io/Microsoft-future-ready-talent-project/"
+                            style={{
+                                background: 'linear-gradient(313deg, #F98214 -15.02%, rgba(249, 130, 20, 0.00) 137.34%)',
+                                borderRadius: '66px',
+                                border: '2px solid #EA760C',
+                            }}
+                        >
+                            <span className="absolute -end-full transition-all group-hover:end-4">
+                                <svg
+                                    className="h-5 w-5 rtl:rotate-180"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                    />
+                                </svg>
+                            </span>
+
+                            <span className="text-sm font-medium transition-all group-hover:me-4" >
+                                Know More
+                            </span>
+                        </a>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
